211e3a715b5875da2cc18a7dff4d6b5cc832686f
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-09-26 17:17:45 +0100

    Create an interface called Rentable with a method called firstPayment that returns a Double.

---
+++ src/LandLord/Rentable.java
@@ -0,0 +1,6 @@
+package LandLord;
+public interface Rentable {
+     // Any class that implements has access to the methods in the interface this is why we don't need to use the keyword implements
+      Double APPRECIATION_FEES = 30.00;
+      Double firstPayment();
+}
